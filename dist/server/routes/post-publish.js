48c38fd856b19d3e15fd96ac481b7b525da4104c
nicolaslabbe <nicolas.labbe@adfab.fr>
2016-06-27 17:18:40 +0200

    patch bug v1.5.3 to latest

--- dist/server/routes/post-publish.js
+++ dist/server/routes/post-publish.js
@@ -10,9 +10,9 @@ var route = function route(req, res, next) {
   _cli.Hooks.instance.trigger('beforeRoute', req, res, next);
   if (typeof res._header !== 'undefined' && res._header !== null) return;
 
-  var filePath = (0, _cli.cleanSlug)(req.body.filePath);
+  var filePath = (0, _cli.cleanSlug)(req.query.filePath);
   var p = new Promise(function (resolve, reject) {
-    (0, _cli.save)(_cli.fileUtils.getFilePath(filePath), req.body.tplPath, req.body.json, '', 'draft', null, 'publish').then(function () {
+    (0, _cli.save)(_cli.fileUtils.getFilePath(filePath), req.query.tplPath, req.query.json, '', 'draft', null, 'publish').then(function () {
       resolve();
     }).catch(function (e) {
       console.error(e.stack);
@@ -20,7 +20,7 @@ var route = function route(req, res, next) {
   });
 
   p.then(function (resSave) {
-    (0, _cli.save)(_cli.fileUtils.getFilePath(req.body.filePath), req.body.tplPath, req.body.json, '', 'publish', resSave, 'publish').then(function (resSave) {
+    (0, _cli.save)(_cli.fileUtils.getFilePath(req.query.filePath), req.query.tplPath, req.query.json, '', 'publish', resSave, 'publish').then(function (resSave) {
       if (typeof resSave.error !== 'undefined' && resSave.error !== null) {
         res.set('Content-Type', 'application/json');
         res.send(JSON.stringify({ error: resSave.error }));
