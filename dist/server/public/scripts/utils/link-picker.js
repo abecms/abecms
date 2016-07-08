f8071996e4dde5b7600dd16d8c4a8b371cf35f80
wonknu10 <fabrice.labbe@adfab.fr>
2016-07-08 14:42:10 +0200

    rich text link has target blank and rel nofollow now

--- dist/server/public/scripts/utils/link-picker.js
+++ dist/server/public/scripts/utils/link-picker.js
@@ -30,6 +30,31 @@ var LinkPicker = function () {
     this.linkNode.value = '';
     this.linkNode.placeholder = 'http://example.com';
 
+    this.checkboxes = document.createElement('div');
+    this.checkboxes.classList.add('coche');
+    this.checkboxes.classList.add('keep-popup');
+
+    this.noFollowLabel = document.createElement('label');
+    this.noFollowLabel.textContent = 'no-follow';
+    this.noFollowLabel.classList.add('keep-popup');
+    this.noFollow = document.createElement('input');
+    this.noFollow.type = "checkbox";
+    this.noFollow.name = "no-follow-" + parseInt(Math.random() * 100);
+    this.noFollow.classList.add('keep-popup');
+
+    this.targetLabel = document.createElement('label');
+    this.targetLabel.textContent = 'target blank';
+    this.targetLabel.classList.add('keep-popup');
+    this.target = document.createElement('input');
+    this.target.type = "checkbox";
+    this.target.name = "target-" + parseInt(Math.random() * 100);
+    this.target.classList.add('keep-popup');
+
+    this.noFollowLabel.appendChild(this.noFollow);
+    this.targetLabel.appendChild(this.target);
+    this.checkboxes.appendChild(this.noFollowLabel);
+    this.checkboxes.appendChild(this.targetLabel);
+
     this.btn = document.createElement('button');
     this.btn.className = 'btn btn-primary';
     this.btn.textContent = 'Add';
@@ -37,6 +62,7 @@ var LinkPicker = function () {
     this.wrapper.innerHTML = '';
     this.wrapper.appendChild(this.linkNode);
     this.wrapper.appendChild(this.btn);
+    this.wrapper.appendChild(this.checkboxes);
     this.onLink = (0, _on2.default)(this);
   }
 
@@ -49,17 +75,21 @@ var LinkPicker = function () {
         _this.wrapper.removeEventListener('mousedown', clickWrapper);
         document.body.removeEventListener('mousedown', cancel);
         _this.wrapper.querySelector('input').value = '';
+        _this.noFollow.checked = false;
+        _this.target.checked = false;
         _this.onLink._fire(res);
         _this.popup.close();
       };
       var cancel = function cancel(e) {
         var target = e.target;
-        if (!target.classList.contains('abe-popup') && !target.parentNode.classList.contains('abe-popup')) sendEvent(null);
+        if (!target.classList.contains('abe-popup') && !target.parentNode.classList.contains('keep-popup') && !target.parentNode.classList.contains('abe-popup')) {
+          sendEvent(null);
+        }
       };
       var clickWrapper = function clickWrapper(e) {
         var link = _this.wrapper.querySelector('input').value;
         if (e.target.classList.contains('btn')) {
-          sendEvent(link);
+          sendEvent({ link: link, target: _this.target.checked, noFollow: _this.noFollow.checked });
         }
       };
       this.wrapper.addEventListener('mousedown', clickWrapper);
--- src/server/public/scripts/utils/link-picker.js
+++ src/server/public/scripts/utils/link-picker.js
@@ -12,6 +12,31 @@ export default class LinkPicker {
     this.linkNode.value = ''
     this.linkNode.placeholder = 'http://example.com'
 
+    this.checkboxes = document.createElement('div')
+    this.checkboxes.classList.add('coche')
+    this.checkboxes.classList.add('keep-popup')
+
+    this.noFollowLabel = document.createElement('label')
+    this.noFollowLabel.textContent = 'no-follow'
+    this.noFollowLabel.classList.add('keep-popup')
+    this.noFollow = document.createElement('input')
+    this.noFollow.type = "checkbox";
+    this.noFollow.name = "no-follow-" + parseInt(Math.random() * 100)
+    this.noFollow.classList.add('keep-popup')
+
+    this.targetLabel = document.createElement('label')
+    this.targetLabel.textContent = 'target blank'
+    this.targetLabel.classList.add('keep-popup')
+    this.target = document.createElement('input')
+    this.target.type = "checkbox";
+    this.target.name = "target-" + parseInt(Math.random() * 100)
+    this.target.classList.add('keep-popup')
+
+    this.noFollowLabel.appendChild(this.noFollow)
+    this.targetLabel.appendChild(this.target)
+    this.checkboxes.appendChild(this.noFollowLabel)
+    this.checkboxes.appendChild(this.targetLabel)
+
     this.btn = document.createElement('button')
     this.btn.className = 'btn btn-primary'
     this.btn.textContent = 'Add'
@@ -19,6 +44,7 @@ export default class LinkPicker {
     this.wrapper.innerHTML = ''
     this.wrapper.appendChild(this.linkNode)
     this.wrapper.appendChild(this.btn)
+    this.wrapper.appendChild(this.checkboxes)
     this.onLink = on(this)
   }
 
@@ -27,17 +53,23 @@ export default class LinkPicker {
       this.wrapper.removeEventListener('mousedown', clickWrapper)
       document.body.removeEventListener('mousedown', cancel)
       this.wrapper.querySelector('input').value = ''
+      this.noFollow.checked = false
+      this.target.checked = false
       this.onLink._fire(res)
       this.popup.close()
     }
     var cancel = (e) => {
       var target = e.target
-      if(!target.classList.contains('abe-popup') && !target.parentNode.classList.contains('abe-popup')) sendEvent(null)
+      if(!target.classList.contains('abe-popup') &&
+         !target.parentNode.classList.contains('keep-popup') &&
+         !target.parentNode.classList.contains('abe-popup')) {
+        sendEvent(null)
+      }
     }
     var clickWrapper = (e) => {
       var link = this.wrapper.querySelector('input').value
       if(e.target.classList.contains('btn')){
-        sendEvent(link)
+        sendEvent({link: link, target: this.target.checked, noFollow: this.noFollow.checked})
       }
     }
     this.wrapper.addEventListener('mousedown', clickWrapper)
