876610104cb85c3f2cfb2bac7ebabc2aae295bc9
wonknu10 <fabrice.labbe@adfab.fr>
2016-07-21 16:37:42 +0200

    fix test if 'value' is null

--- dist/cli/handlebars/abe/compileAbe.js
+++ dist/cli/handlebars/abe/compileAbe.js
@@ -33,7 +33,7 @@ function compileAbe() {
     key = key[key.length - 1];
     var hash = arguments[0].hash;
     hash.key = hash.key.replace(/\{\{@index\}\}/, '[{{@index}}]');
-    var value = (content ? content[hash['dictionnary']][arguments[0].data.index][key] : hash.key).replace(/%27/, "'");
+    var value = content ? content[hash['dictionnary']][arguments[0].data.index][key] : hash.key;
     if (typeof hash.type !== 'undefined' && hash.type !== null && hash.type === 'rich') {
       var testXSS = (0, _xss2.default)(value.replace(/&quot;/g, '"'), {
         "whiteList": _.config.htmlWhiteList,
@@ -41,13 +41,13 @@ function compileAbe() {
       });
       return new _handlebars2.default.SafeString(testXSS);
     }
-    return value;
+    return value.replace(/%27/, "'");
   }
 
   var key = arguments[0].hash['key'].replace('.', '-');
 
   var hash = arguments[0].hash;
-  var value = (content ? content[hash.key.replace('.', '-')] : hash.key).replace(/%27/, "'");
+  var value = content ? content[hash.key.replace('.', '-')] : hash.key;
   if (typeof value === 'undefined' || value === null) {
     value = '';
   }
@@ -59,5 +59,5 @@ function compileAbe() {
     });
     return new _handlebars2.default.SafeString(testXSS);
   }
-  return value;
+  return value.replace(/%27/, "'");
 }
