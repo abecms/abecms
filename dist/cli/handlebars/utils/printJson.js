48dbfd0b0132e9c3635d44b46eaee48167fb221d
nicolaslabbe <nicolas.labbe@adfab.fr>
2016-07-21 11:53:24 +0200

    bug printjson

--- dist/cli/handlebars/utils/printJson.js
+++ dist/cli/handlebars/utils/printJson.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true
 });
 exports.default = printJson;
 
@@ -9,5 +9,9 @@ exports.default = printJson;
  * Handlebars helper, to print json object
  */
 function printJson(obj, escapeString) {
-  return typeof escapeString !== null && escapeString !== null && escapeString === 1 ? escape(JSON.stringify(obj).replace(/'/g, "%27")) : JSON.stringify(obj).replace(/'/g, "%27");
+	if (typeof obj !== 'undefined' && obj !== null) {
+		return typeof escapeString !== null && escapeString !== null && escapeString === 1 ? escape(JSON.stringify(obj).replace(/'/g, "%27")) : JSON.stringify(obj).replace(/'/g, "%27");
+	} else {
+		return '{}';
+	}
 }
