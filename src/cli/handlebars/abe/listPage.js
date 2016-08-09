6dbfad534b2c29e4c1b318c22125254cd6a0f9f4
wonknu10 <fabrice.labbe@adfab.fr>
2016-08-09 14:16:35 +0200

    remove hidden label inside page list

--- src/cli/handlebars/abe/listPage.js
+++ src/cli/handlebars/abe/listPage.js
@@ -33,7 +33,7 @@ export default function listPage(file, index, text) {
   if((typeof file.published !== undefined && file.published !== null && !file.published) || (file.published && file.draft && file.published.date < file.draft.date)) {
     workflow += `<a href="/abe/${file.template}?filePath=${file.path}" class="label label-default label-draft">draft</a>`
   }else {
-    workflow += `<a href="/abe/${file.template}?filePath=${file.path}" class="hidden label label-default label-draft">draft</a>`
+    workflow += `<a href="/abe/${file.template}?filePath=${file.path}" class="hidden label label-default label-draft"></a>`
   }
 
   workflow += `</td>`
