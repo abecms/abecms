d6cd85ab5082aaa937720c945c1853bc2fa1f4ee
wonknu10 <fabrice.labbe@adfab.fr>
2016-08-11 14:08:29 +0200

    build command for prod file

--- src/cli/Builder.js
+++ src/cli/Builder.js
@@ -20,18 +20,43 @@ class Builder {
   constructor(root, folder, dest, flow){
     this.pathToJson = fileUtils.concatPath(root, config.data.url)
     var files = fileAttr.filterLatestVersion(FileParser.getFiles(this.pathToJson, config.data.url), flow)
-    files.forEach(function (file) {
-      var json = fse.readJsonSync(file.path)
-      var text = getTemplate(json.abe_meta.template)
 
-      Util.getDataList(fileUtils.removeLast(json.abe_meta.link), text, json)
-      .then(() => {
-        var page = new Page(json.abe_meta.link, text, json, true)
-        saveHtml(fileUtils.concatPath(root, dest + json.abe_meta.link), page.html)
-        console.log(fileUtils.concatPath(root, dest + json.abe_meta.link))
-      }).catch(function(e) {
-        console.error(e)
-      })
+    if(flow === 'publish') {
+      files = FileParser.getFiles(fileUtils.concatPath(root, config.publish.url), new RegExp('.' + config.files.templates.extension))
+      // files = FileParser.getMetas(files, 'draft')
+    }
+
+    files.forEach(function (file) {
+      if(file.path.indexOf('.' + config.files.templates.extension) > -1){
+        console.log(1, file.path)
+        file.path = file.path.replace(config.publish.url, config.data.url)
+                             .replace('.' + config.files.templates.extension, '.json')
+        
+        var json = fse.readJsonSync(file.path)
+        var text = getTemplate(json.abe_meta.template)
+        
+        Util.getDataList(fileUtils.removeLast(json.abe_meta.link), text, json)
+        .then(() => {
+          var page = new Page(json.abe_meta.link, text, json, true)
+          saveHtml(fileUtils.concatPath(root, dest + json.abe_meta.link), page.html)
+          console.log(fileUtils.concatPath(root, dest + json.abe_meta.link))
+        }).catch(function(e) {
+          console.error(e)
+        })
+      }
+      else if(file.path.indexOf('.json') > -1){
+        var json = fse.readJsonSync(file.path)
+        var text = getTemplate(json.abe_meta.template)
+
+        Util.getDataList(fileUtils.removeLast(json.abe_meta.link), text, json)
+        .then(() => {
+          var page = new Page(json.abe_meta.link, text, json, true)
+          saveHtml(fileUtils.concatPath(root, dest + json.abe_meta.link), page.html)
+          console.log(fileUtils.concatPath(root, dest + json.abe_meta.link))
+        }).catch(function(e) {
+          console.error(e)
+        })
+      }
     })
   }
 
