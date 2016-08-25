fb53862646bee879ba70e5c47045e34eb116acb3
gregorybesson <gregory.besson@adfab.fr>
2016-08-25 12:26:42 +0200

    ABE-166

--- dist/tasks/sass.js
+++ dist/tasks/sass.js
@@ -12,16 +12,16 @@ var result = sass.render({
   outFile: output,
   sourceMap: true
 }, function (error, result) {
-  // node-style callback from v3.0.0 onwards
+  // node-style callback from v3.0.0 onwards 
   if (!error) {
     console.log(clc.green('write sass ' + output));
-    // No errors during the compilation, write this result on the disk
+    // No errors during the compilation, write this result on the disk 
     fs.writeFile(output, result.css, function (err) {
       if (!err) {
-        //file written on disk
+        //file written on disk 
       }
     });
   } else {
-      console.log(clc.red('ERROR ' + error));
-    }
+    console.log(clc.red('ERROR ' + error));
+  }
 });
