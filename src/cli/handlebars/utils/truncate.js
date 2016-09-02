d9dde84818aba01c6f71874c256ec73d6ff7c5e2
gregorybesson <gregory.besson@adfab.fr>
2016-09-02 14:56:06 +0200

    fix truncate when passed value is null

--- src/cli/handlebars/utils/truncate.js
+++ src/cli/handlebars/utils/truncate.js
@@ -1,7 +1,7 @@
 import Handlebars from 'handlebars'
 
 export default function truncate(str, len) {
-  if (str && str.length > len) {
+  if (typeof str !== 'undefined' && str.length > len) {
     new_str = new Handlebars.SafeString (str);
     var new_str = str + " ";
     new_str = str.substr (0, len);
@@ -9,6 +9,9 @@ export default function truncate(str, len) {
     new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
 
     return new_str +'...'; 
+  } else {
+    
+    return '';
   }
   
   return new Handlebars.SafeString (str);
