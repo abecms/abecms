cb5cc03430fc0e9484077a1a7a4442a6bb9dbc04
gregorybesson <gregory.besson@adfab.fr>
2016-09-06 16:18:44 +0200

    ABE-171

--- src/cli/handlebars/utils/isTrue.js
+++ src/cli/handlebars/utils/isTrue.js
@@ -14,9 +14,19 @@ export default function isTrue(v1, operator, v2) {
     case '>=':
       return (v1 >= v2);
     case '&&':
-      return (v1 && v2);
+      var eval1 = false;
+      var eval2 = false;
+      if((v1 && !Array.isArray(v1)) || v1.length>0) eval1 = true;
+      if((v2 && !Array.isArray(v2))  || v2.length>0) eval2 = true;
+
+      return (eval1 && eval2);
     case '||':
-      return (v1 || v2);
+      var eval1 = false;
+      var eval2 = false;
+      if((v1 && !Array.isArray(v1)) || v1.length>0) eval1 = true;
+      if((v2 && !Array.isArray(v2))  || v2.length>0) eval2 = true;
+
+      return (eval1 || eval2);
     default:
       return false;
   }
