d9510567022d485e3049082225c67a0a0f1db660
gregorybesson <gregory.besson@adfab.fr>
2016-09-12 16:44:31 +0200

    fix isTrue handlebars helper

--- src/cli/handlebars/utils/isTrue.js
+++ src/cli/handlebars/utils/isTrue.js
@@ -16,15 +16,15 @@ export default function isTrue(v1, operator, v2) {
     case '&&':
       var eval1 = false;
       var eval2 = false;
-      if((v1 && !Array.isArray(v1)) || (Array.isArray(v1) && v1.length>0)) eval1 = true;
-      if((v2 && !Array.isArray(v2)) || (Array.isArray(v2) && v1.length>0)) eval2 = true;
+      if((v1 && !Array.isArray(v1)) || (Array.isArray(v1) && v1.length>0) || typeof v1 === 'object') eval1 = true;
+      if((v2 && !Array.isArray(v2)) || (Array.isArray(v2) && v2.length>0) || typeof v2 === 'object') eval2 = true;
 
       return (eval1 && eval2);
     case '||':
       var eval1 = false;
       var eval2 = false;
-      if((v1 && !Array.isArray(v1)) || (Array.isArray(v1) && v1.length>0)) eval1 = true;
-      if((v2 && !Array.isArray(v2)) || (Array.isArray(v2) && v1.length>0)) eval2 = true;
+      if((v1 && !Array.isArray(v1)) || (Array.isArray(v1) && v1.length>0) || typeof v1 === 'object') eval1 = true;
+      if((v2 && !Array.isArray(v2)) || (Array.isArray(v2) && v1.length>0) || typeof v2 === 'object') eval2 = true;
 
       return (eval1 || eval2);
     default:
