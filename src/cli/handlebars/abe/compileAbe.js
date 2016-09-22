c47e3acab6fed09e6a5e81d716807e5a0fce4f46
wonknu10 <fabrice.labbe@adfab.fr>
2016-09-22 17:36:28 +0200

    value not exist

--- src/cli/handlebars/abe/compileAbe.js
+++ src/cli/handlebars/abe/compileAbe.js
@@ -18,7 +18,13 @@ export default function compileAbe(){
     key = key[key.length - 1]
     var hash = arguments[0].hash
     hash.key = hash.key.replace(/\{\{@index\}\}/, '[{{@index}}]')
-    var value = ((content && content[hash['dictionnary']]) ? content[hash['dictionnary']][arguments[0].data.index][key] : hash.key)
+    var value
+    try{
+      value = content ? content[hash['dictionnary']][arguments[0].data.index][key] : hash.key;
+    }
+    catch(e){
+      value = ''
+    }
     if(typeof value === 'undefined' || typeof value === 'function' || value === null) {
       value = ''
     }
