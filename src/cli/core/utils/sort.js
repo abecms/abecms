4e769efa4e7e65963a5a4dc4a079b174060f3c88
nicolaslabbe <nicolas.labbe@adfab.fr>
2016-10-04 15:37:03 +0200

    add comment sort.js

--- src/cli/core/utils/sort.js
+++ src/cli/core/utils/sort.js
@@ -1,3 +1,12 @@
+/**
+ * sort an array of objects by date attribute
+ *
+ * Example : [{date: date}, {date: date}].sort(coreUtils.sort.byDateDesc)
+ * 
+ * @param  {Object} a object with date attribute
+ * @param  {Object} b object with date attribute
+ * @return {Int}   1 | 0 | -1
+ */
 export function byDateDesc(a, b) {
   var dateA = new Date(a.date)
   var dateB = new Date(b.date)
@@ -9,6 +18,14 @@ export function byDateDesc(a, b) {
   return 0
 }
 
+/**
+ * shuffle an array of objects by date attribute
+ *
+ * Example : var shuffledArray = coreUtils.sort.shuffle([Object}, Object])
+ * 
+ * @param  {Array} array
+ * @return {Array} array
+ */
 export function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex
 
@@ -27,7 +44,15 @@ export function shuffle(array) {
 
   return array
 }
-
+/**
+ * sort an array of objects by date attribute
+ *
+ * Example : [{date: date}, {date: date}].sort(coreUtils.sort.byDateAsc)
+ * 
+ * @param  {Object} a object with date attribute
+ * @param  {Object} b object with date attribute
+ * @return {Int}   1 | 0 | -1
+ */
 export function byDateAsc(a, b) {
   var dateA = new Date(a.date)
   var dateB = new Date(b.date)
