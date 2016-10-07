39b560211cb624d3cda8e893b8eac9580c2ea5c0
gregorybesson <gregory.besson@adfab.fr>
2016-10-07 23:57:15 +0200

    refactoring: dead code

--- src/cli/core/utils/slug.js
+++ src/cli/core/utils/slug.js
@@ -7,9 +7,6 @@ import {
 export function clean(str) {
 
   if (typeof str === 'undefined' || str === null) return null
-  if (str.indexOf('.') === -1) { // no extension add one
-    str = `${str}.${config.files.templates.extension}`
-  }
   str = str.split('/')
   str[str.length - 1] = slugify(decodeURIComponent(str[str.length - 1]))
   return str.join('/')
