bd9e46b9651462c15cca195fe2fbbe79c8ae26b6
gregorybesson <gregory.besson@adfab.fr>
2016-10-10 16:32:58 +0200

    fix a miserable error removing functions... which is mandatory

--- src/cli/core/utils/file.js
+++ src/cli/core/utils/file.js
@@ -22,3 +22,18 @@ export function changePath(pathEnv, change) {
 
   return path.join(config.root, pathEnv.join('/'))
 }
+
+/**
+ * This method checks that the path leads to a file and return the content as UTF-8 content
+ * @param  {string} path The path
+ * @return {string}      The content of the UTF-8 file
+ */
+export function getContent(pathFile) {
+  var res = null
+  if(typeof pathFile !== 'undefined' && pathFile !== null && pathFile !== '') {
+    if (exist(pathFile)) {
+      res = fse.readFileSync(pathFile, 'utf8')
+    }
+  }
+  return res
+}
