72dc975863870127254610c67d7eea465ae7b53a
gregorybesson <gregory.besson@adfab.fr>
2016-10-17 09:09:50 +0200

    refactoring: Publish action returns to the lib

--- src/server/routes/post-publish.js
+++ src/server/routes/post-publish.js
@@ -1,6 +1,5 @@
 import path from 'path'
 import {
-  config,
   cmsOperations,
   abeExtend,
   Manager
@@ -10,54 +9,21 @@ var route = function(req, res, next){
   abeExtend.hooks.instance.trigger('beforeRoute', req, res, next)
   if(typeof res._header !== 'undefined' && res._header !== null) return
 
-  var p = new Promise((resolve) => {
-    cmsOperations.save.save(
-      path.join(config.root, config.draft.url, req.body.filePath.replace(config.root)),
-      req.body.tplPath,
-      req.body.json,
-      '',
-      'draft',
-      null,
-      'publish')
-      .then(() => {
-        resolve()
-      }).catch(function(e) {
-        console.error(e)
-      })
-  })
+  var p = cmsOperations.post.publish(
+    req.body.filePath, 
+    req.body.tplPath,
+    req.body.json
+  );
 
-  p.then((resSave) => {
-    cmsOperations.save.save(
-      path.join(config.root, config.draft.url, req.body.filePath.replace(config.root)),
-      req.body.tplPath,
-      req.body.json,
-      '',
-      'publish',
-      resSave,
-      'publish')
-      .then((resSave) => {
-        if(typeof resSave.error !== 'undefined' && resSave.error !== null  ){
-          res.set('Content-Type', 'application/json')
-          res.send(JSON.stringify({error: resSave.error}))
-        }
-        var result
-        if(typeof resSave.reject !== 'undefined' && resSave.reject !== null){
-          result = resSave
-        }
-        if(typeof resSave.json !== 'undefined' && resSave.json !== null){
-          result = {
-            success: 1,
-            json: resSave.json
-          }
-        }
-        Manager.instance.updateList()
-        res.set('Content-Type', 'application/json')
-        res.send(JSON.stringify(result))
-      }).catch(function(e) {
-        console.error('post-publish.js', e)
-      })
+  p.then((result) => {
+    res.set('Content-Type', 'application/json')
+    res.send(JSON.stringify(result))
+  },
+  (result) => {
+    res.set('Content-Type', 'application/json')
+    res.send(JSON.stringify(result))
   }).catch(function(e) {
-    console.error('post-publish.js', e)
+    console.error('[ERROR] post-publish.js', e)
   })
 }
 
