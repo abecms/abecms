5926db146e12f79de124115a32c30ee9f12e527d
gregorybesson <gregory.besson@adfab.fr>
2016-10-17 09:22:23 +0200

    refactoring: Reject action put in the lib

--- src/server/routes/post-reject.js
+++ src/server/routes/post-reject.js
@@ -9,52 +9,22 @@ import {
 var route = function(req, res, next){
   abeExtend.hooks.instance.trigger('beforeRoute', req, res, next)
   if(typeof res._header !== 'undefined' && res._header !== null) return
-  var p = new Promise((resolve) => {
-    cmsOperations.save.save(
-      path.join(config.root, config.draft.url, req.body.filePath.replace(config.root)),
-      req.body.tplPath,
-      req.body.json,
-      '',
-      'draft',
-      null,
-      'reject')
-      .then(() => {
-        resolve()
-      }).catch(function(e) {
-        console.error(e)
-      })
-  })
+  
+  var p = cmsOperations.post.reject(
+    req.body.filePath, 
+    req.body.tplPath,
+    req.body.json
+  );
 
-  p.then((resSave) => {
-    cmsOperations.save.save(
-      path.join(config.root, config.draft.url, req.body.filePath.replace(config.root)),
-      req.body.tplPath,
-      req.body.json,
-      '',
-      'reject',
-      resSave,
-      'reject')
-      .then((resSave) => {
-        if(typeof resSave.error !== 'undefined' && resSave.error !== null  ){
-          res.set('Content-Type', 'application/json')
-          res.send(JSON.stringify({error: resSave.error}))
-        }
-        var result
-        if(typeof resSave.reject !== 'undefined' && resSave.reject !== null){
-          result = resSave
-        }
-        if(typeof resSave.json !== 'undefined' && resSave.json !== null){
-          result = {
-            success: 1,
-            json: resSave.json
-          }
-        }
-        Manager.instance.updateList()
-        res.set('Content-Type', 'application/json')
-        res.send(JSON.stringify(result))
-      })
+  p.then((result) => {
+    res.set('Content-Type', 'application/json')
+    res.send(JSON.stringify(result))
+  },
+  (result) => {
+    res.set('Content-Type', 'application/json')
+    res.send(JSON.stringify(result))
   }).catch(function(e) {
-    console.error(e)
+    console.error('[ERROR] post-reject.js', e)
   })
 }
 
