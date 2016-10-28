e190d84ae647b9cf4f18b827f0b4744282f01fa0
nicolaslabbe <nicolas.labbe@adfab.fr>
2016-10-28 09:00:24 +0200

    refactor: remove logs + bug: on create

--- src/server/public/scripts/modules/FormCreate.js
+++ src/server/public/scripts/modules/FormCreate.js
@@ -144,7 +144,7 @@ export default class FormCreate {
         (code, responseText) => {
           var jsonRes = JSON.parse(responseText)
           if (jsonRes.success == 1 && typeof jsonRes.json.abe_meta !== 'undefined' && jsonRes.json.abe_meta !== null) {
-            window.location.href = window.location.origin + '/abe/' + jsonRes.json.abe_meta.template + '?filePath=' + jsonRes.json.abe_meta.link
+            window.location.href = window.location.origin + '/abe' + jsonRes.json.abe_meta.link
           }else {
             alert('error')
             btn.classList.remove('disable')
