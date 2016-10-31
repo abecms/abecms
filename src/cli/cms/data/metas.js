c825726a39d6d17b46cdabeff26730758530d72a
gregorybesson <gregory.besson@adfab.fr>
2016-10-31 16:39:58 +0100

    refactoring:removing dead code

--- src/cli/cms/data/metas.js
+++ src/cli/cms/data/metas.js
@@ -26,27 +26,3 @@ export function add(tpl, json, type, obj = {}, date = null, realType = 'draft') 
   json[meta][type].latest.date = currentDate
   json[meta][type].latest.abeUrl = abeUrl
 }
-
-export function get(arr) {
-  var res = []
-  Array.prototype.forEach.call(arr, (file) => {
-    let meta = config.meta.name
-
-    var jsonPath = cmsData.file.fromUrl(file.path).json.path
-    var json = cmsData.file.get(jsonPath)
-    if(json[meta] == null) json[meta] = {}
-    file['template'] = json[meta].template
-    if(json[meta].latest != null) {
-      file['date'] = json[meta].latest.date
-    }
-    if(json[meta].complete == null) {
-      json[meta].complete = 0
-    }
-    if(json[meta] != null) {
-      file[config.meta.name] = json[meta]
-    }
-    res.push(file)
-  })
-
-  return res
-}
