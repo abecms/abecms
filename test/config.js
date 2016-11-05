7f643067c82c7d5b144ecd64f55c6663e56d61d8
gregorybesson <gregory.besson@adfab.fr>
2016-11-05 13:27:03 +0100

    fix UT config on Windows

--- test/config.js
+++ test/config.js
@@ -11,7 +11,7 @@ describe('Config', function() {
    */
   it('configuration file', function() {
     chai.assert.equal(config.localeFolder, 'locale', 'Error on config file')
-    chai.assert.equal(config.root.substr(config.root.lastIndexOf('/') + 1), 'fixtures', 'Error on config file')
+    chai.assert.equal(config.root.substr(config.root.lastIndexOf(path.sep) + 1), 'fixtures', 'Error on config file')
     chai.assert.equal(config.upload.image, 'unitimage', 'Error on config file')
   });
 });
