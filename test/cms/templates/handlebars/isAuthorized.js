81bc0cca1e0ddbae211797ecf0663553055b3445
nicolaslabbe <nicolas.labbe@adfab.fr>
2016-11-28 16:54:05 +0100

    UT: template (stub/unstub)

---
+++ test/cms/templates/handlebars/isAuthorized.js
@@ -0,0 +1,38 @@
+var chai = require('chai');
+var sinonChai = require('sinon-chai')
+var expect = chai.expect
+chai.use(sinonChai)
+var sinon = require('sinon');
+var fs = require('fs-extra');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var bcrypt = require('bcrypt-nodejs');
+var Cookies = require('cookies');
+var jwt = require('jwt-simple');
+var Handlebars =require('../../../../src/cli').Handlebars
+
+var coreUtils = require('../../../../src/cli').coreUtils
+var config = require('../../../../src/cli').config
+config.set({root: path.join(process.cwd(), 'test', 'fixtures')})
+
+var User = require('../../../../src/cli').User;
+describe('Handlebars.helpers.isAuthorized', function() {
+  before( function() {
+    config.users.enable = true
+    this.fixture = {
+      htmlIsAuthorized: fs.readFileSync(path.join(process.cwd(), 'test', 'fixtures', 'templates', 'isAuthorized.html'), 'utf8'),
+      htmlIsAuthorizedTrue: fs.readFileSync(path.join(process.cwd(), 'test', 'fixtures', 'templates', 'isAuthorizedTrue.html'), 'utf8'),
+      users: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'test', 'fixtures', 'users', 'users.json'), 'utf8'))
+    }
+  });
+
+  it('Handlebars.helpers.isAuthorized', function() {
+    var template = Handlebars.compile(this.fixture.htmlIsAuthorized)
+    var resHtml = template({})
+    chai.expect(resHtml).to.be.equal("");
+
+    template = Handlebars.compile(this.fixture.htmlIsAuthorizedTrue)
+    resHtml = template({})
+    chai.expect(resHtml).to.not.be.equal("");
+  });
+});
---
+++ test/users/manager.js
@@ -0,0 +1,86 @@
+var chai = require('chai');
+var sinonChai = require('sinon-chai')
+var expect = chai.expect
+chai.use(sinonChai)
+var sinon = require('sinon');
+var fs = require('fs-extra');
+var path = require('path');
+
+var coreUtils = require('../../src/cli').coreUtils
+var config = require('../../src/cli').config
+config.set({root: path.join(process.cwd(), 'test', 'fixtures')})
+
+var User = require('../../src/cli').User;
+
+describe('User.manager', function() {
+  before( function() {
+    config.users.enable = true
+    this.fixture = {
+      htmlIsAuthorized: fs.readFileSync(path.join(process.cwd(), 'test', 'fixtures', 'templates', 'isAuthorized.html'), 'utf8'),
+      htmlIsAuthorizedTrue: fs.readFileSync(path.join(process.cwd(), 'test', 'fixtures', 'templates', 'isAuthorizedTrue.html'), 'utf8'),
+      users: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'test', 'fixtures', 'users', 'users.json'), 'utf8'))
+    }
+  });
+
+  it('User.manager.instance.get', function(){
+    // stub
+    var sinonInstance = sinon.sandbox.create();
+    var stubRead = sinonInstance.stub(User.manager.instance, 'read');
+    stubRead.returns(JSON.parse(JSON.stringify(this.fixture.users)))
+
+    // test
+    var res = User.manager.instance.get()
+    chai.expect(res.length).to.be.above(0)
+
+    // unstub
+    User.manager.instance.read.restore()
+  })
+
+  it('User.manager.instance.save', function(){
+    // stub
+    var sinonInstance = sinon.sandbox.create();
+    var stubFs = sinonInstance.stub(fs, 'writeJsonSync');
+    stubFs.returns(null)
+
+    // test
+    var res = User.manager.instance.save()
+
+    // unstub
+    sinon.assert.calledOnce(fs.writeJsonSync)
+    fs.writeJsonSync.restore()
+  })
+
+  it('User.manager.instance.read', function(){
+    // stub
+    var sinonInstance = sinon.sandbox.create();
+    var stubFs = sinonInstance.stub(fs, 'readFileSync');
+    stubFs.returns(JSON.stringify(this.fixture.users))
+    var stubExist = sinonInstance.stub(coreUtils.file, 'exist');
+    stubExist.returns(true)
+
+    // test
+    var res = User.manager.instance.read()
+    chai.expect(res).to.not.be.null
+
+    // unstub
+    sinon.assert.calledOnce(fs.readFileSync)
+    fs.readFileSync.restore()
+    sinon.assert.calledOnce(coreUtils.file.exist)
+    coreUtils.file.exist.restore()
+  })
+
+  it('User.manager.instance.update', function(){
+    // stub
+    var sinonInstance = sinon.sandbox.create();
+    var stubSave = sinonInstance.stub(User.manager.instance, 'save');
+    stubSave.returns(JSON.parse(JSON.stringify(this.fixture.users)))
+
+    // test
+    var res = User.manager.instance.update()
+    chai.expect(res).to.be.equal(true)
+
+    // unstub
+    sinon.assert.calledOnce(User.manager.instance.save)
+    User.manager.instance.save.restore()
+  })
+});
