867b9663f734a6e2ee3269d3a08b5749adcbde4b
nicolaslabbe <nicolas.labbe@adfab.fr>
2016-12-02 17:59:47 +0100

    enhancement: add list-workflow with better explanation + bug url unpublish

--- src/server/routes/operations/get/unpublish.js
+++ src/server/routes/operations/get/unpublish.js
@@ -8,13 +8,13 @@ var route = function(req, res, next){
   abeExtend.hooks.instance.trigger('beforeRoute', req, res, next)
   if(typeof res._header !== 'undefined' && res._header !== null) return
 
-  var operation = req.originalUrl.replace(/\/abe\/operations\/unpublish\//, '')
+  var postUrl = req.originalUrl.replace(/\/abe\/operations\/unpublish\//, '')
 
-  cmsOperations.post.unpublish(operation.postUrl)
+  cmsOperations.post.unpublish(postUrl)
 
   var result = {
     success: 1,
-    file: operation.postUrl
+    file: postUrl
   }
   res.set('Content-Type', 'application/json')
   res.send(JSON.stringify(result))
