10f5a25c57b34b7d60e2de0a76f697f1a96a3b12
gregorybesson <gregory.besson@adfab.fr>
2016-12-09 18:35:30 +0100

    fix: The assets (images / css / js) of Abe were grabbed before thse from the templates directory producing bugs

--- src/server/middlewares/login.js
+++ src/server/middlewares/login.js
@@ -4,7 +4,7 @@ var middleware = function(req, res, next) {
     res.send = function (string) {
       var body = string instanceof Buffer ? string.toString() : string
       body = body.replace(/<\/body>/g, function (w) {
-        return '<input type=\'hidden\' id=\'globalCsrfToken\' value=\'' + res.locals._csrf + '\' /><script src=\'/scripts/user-login-compiled.js\'></script>' + w
+        return '<input type=\'hidden\' id=\'globalCsrfToken\' value=\'' + res.locals._csrf + '\' /><script src=\'/abejs/scripts/user-login-compiled.js\'></script>' + w
       })
       body = body.replace(/<\/form>/g, function (w) {
         return '<input type=\'hidden\' name=\'_csrf\' value=\'' + res.locals._csrf + '\' />' + w
