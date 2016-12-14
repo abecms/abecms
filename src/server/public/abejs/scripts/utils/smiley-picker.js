17de5a29d37e97cfb6dda9219e3432bd2d56ce5d
wonknu10 <fabrice.labbe@adfab.fr>
2016-12-14 14:28:33 +0100

    UT: test core utils files

--- src/server/public/abejs/scripts/utils/smiley-picker.js
+++ src/server/public/abejs/scripts/utils/smiley-picker.js
@@ -1,91 +1,32 @@
 import on from 'on'
 import Popup from './popup'
+import SmileyData from './smileys' // @Source: http://apps.timwhitlock.info/emoji/tables/unicode
 
 export default class SmileyPicker {
-
   constructor(wrapper) {
     this.popup = new Popup(wrapper)
     this.wrapper = wrapper
-    this.smileys = [
-      {desc: "grinning face with smiling eyes", icon: "😁"},
-      {desc: "face with tears of joy", icon: "😂"},
-      {desc: "smiling face with open mouth", icon: "😃"},
-      {desc: "smiling face with open mouth and smiling eyes", icon: "😄"},
-      {desc: "smiling face with open mouth and cold sweat", icon: "😅"},
-      {desc: "smiling face with open mouth and tightly-closed eyes", icon: "😆"},
-      {desc: "winking face", icon: "😉"},
-      {desc: "smiling face with smiling eyes", icon: "😊"},
-      {desc: "face savouring delicious food", icon: "😋"},
-      {desc: "relieved face", icon: "😌"},
-      {desc: "smiling face with heart-shaped eyes", icon: "😍"},
-      {desc: "smirking face", icon: "😏"},
-      {desc: "unamused face", icon: "😒"},
-      {desc: "face with cold sweat", icon: "😓"},
-      {desc: "pensive face", icon: "😔"},
-      {desc: "confounded face", icon: "😖"},
-      {desc: "face throwing a kiss", icon: "😘"},
-      {desc: "kissing face with closed eyes", icon: "😚"},
-      {desc: "face with stuck-out tongue and winking eye", icon: "😜"},
-      {desc: "face with stuck-out tongue and tightly-closed eyes", icon: "😝"},
-      {desc: "disappointed face", icon: "😞"},
-      {desc: "angry face", icon: "😠"},
-      {desc: "pouting face", icon: "😡"},
-      {desc: "crying face", icon: "😢"},
-      {desc: "persevering face", icon: "😣"},
-      {desc: "face with look of triumph", icon: "😤"},
-      {desc: "disappointed but relieved face", icon: "😥"},
-      {desc: "fearful face", icon: "😨"},
-      {desc: "weary face", icon: "😩"},
-      {desc: "sleepy face", icon: "😪"},
-      {desc: "tired face", icon: "😫"},
-      {desc: "loudly crying face", icon: "😭"},
-      {desc: "face with open mouth and cold sweat", icon: "😰"},
-      {desc: "face screaming in fear", icon: "😱"},
-      {desc: "astonished face", icon: "😲"},
-      {desc: "flushed face", icon: "😳"},
-      {desc: "dizzy face", icon: "😵"},
-      {desc: "face with medical mask", icon: "😷"},
-      {desc: "grinning cat face with smiling eyes", icon: "😸"},
-      {desc: "cat face with tears of joy", icon: "😹"},
-      {desc: "smiling cat face with open mouth", icon: "😺"},
-      {desc: "smiling cat face with heart-shaped eyes", icon: "😻"},
-      {desc: "cat face with wry smile", icon: "😼"},
-      {desc: "kissing cat face with closed eyes", icon: "😽"},
-      {desc: "pouting cat face", icon: "😾"},
-      {desc: "crying cat face", icon: "😿"},
-      {desc: "weary cat face", icon: "🙀"},
-      {desc: "face with no good gesture", icon: "🙅"},
-      {desc: "face with ok gesture", icon: "🙆"},
-      {desc: "person bowing deeply", icon: "🙇"},
-      {desc: "see-no-evil monkey", icon: "🙈"},
-      {desc: "hear-no-evil monkey", icon: "🙉"},
-      {desc: "speak-no-evil monkey", icon: "🙊"},
-      {desc: "happy person raising one hand", icon: "🙋"},
-      {desc: "person raising both hands in celebration", icon: "🙌"},
-      {desc: "person frowning", icon: "🙍"},
-      {desc: "person with pouting face", icon: "🙎"},
-      {desc: "person with folded hands", icon: "🙏"}
-    ]
-
-    var countSmiley = 0;
-    var smileyHTML = `<table cellpadding="0" cellspacing="0">
+    var smileyHTML = '<div class="smiley-title">Emoji</div>'
+    for(var smileys in SmileyData){
+      var countSmiley = 0;
+      smileyHTML += `<div class="smiley-subtitle">${smileys} :</div>
+                    <table cellpadding="0" cellspacing="0">
                         <tbody>
                           <tr>`
-
-    this.smileys.forEach((smiley) => {
-      if(countSmiley > 9) {
-        smileyHTML += '</tr><tr>'
-        countSmiley = 0
-      }
-      smileyHTML += `<td class="wysiwyg-toolbar-smiley" data-smiley="${smiley.icon}">
-                      <span class="" title="${smiley.desc}" data-smiley="${smiley.icon}">${smiley.icon}</span>
-                     </td>`
-       countSmiley++
-    })
-    smileyHTML +=     `</tr>
+      SmileyData[smileys].forEach((smiley) => {
+        if(countSmiley > 9) {
+          smileyHTML += '</tr><tr>'
+          countSmiley = 0
+        }
+        smileyHTML += `<td class="wysiwyg-toolbar-smiley" data-smiley="${smiley.icon}">
+                        <span class="" title="${smiley.desc}" data-smiley="${smiley.icon}">${smiley.icon}</span>
+                       </td>`
+         countSmiley++
+      })
+      smileyHTML +=     `</tr>
                       </tbody>
                     </table>`
-
+    }
     this.wrapper.innerHTML = smileyHTML
     this.bindEvt()
   }
@@ -105,5 +46,4 @@ export default class SmileyPicker {
     var elBounds = el.getBoundingClientRect()
     this.popup.open(elBounds.left, (elBounds.top + elBounds.height + 5))
   }
-
 }
---
+++ test/core/utils/array.js
@@ -0,0 +1,70 @@
+import chai from 'chai'
+import path from 'path'
+import sinonChai from'sinon-chai'
+chai.use(sinonChai)
+import sinon from 'sinon'
+
+import {
+  coreUtils,
+  abeExtend,
+  cmsData,
+  config
+} from '../../../src/cli'
+config.set({root: path.join(process.cwd(), 'test', 'fixtures')})
+
+describe('coreUtils.array', function() {
+
+  var arr = [{'test': 'val'}, {'test2': 'val2'}];
+
+  /**
+   * coreUtils.array.find
+   * 
+   */
+  it('coreUtils.array.find()', function() {
+    var result = coreUtils.array.find(arr, 'test', 'val');
+    chai.expect(result).to.be.a('array')
+    chai.expect(result.length).to.equal(1)
+    chai.expect(result[0]).to.equal(0)
+    var result2 = coreUtils.array.find(arr, 'test2', 'val2');
+    chai.expect(result2).to.be.a('array')
+    chai.expect(result2.length).to.equal(1)
+    chai.expect(result2[0]).to.equal(1)
+  });
+
+  /**
+   * coreUtils.array.filter
+   * 
+   */
+  it('coreUtils.array.filter()', function() {
+    var result = coreUtils.array.filter(arr, 'test', 'val');
+    chai.expect(result).to.be.a('array')
+    chai.expect(result.length).to.equal(1)
+    chai.expect(result[0]).to.have.test
+    chai.expect(result[0].test).to.equal('val')
+  });
+
+  /**
+   * coreUtils.array.removeByAttr
+   * 
+   */
+  it('coreUtils.array.removeByAttr()', function() {
+    var result = coreUtils.array.removeByAttr(arr, 'test2', 'val2');
+    chai.expect(result).to.be.a('array')
+    chai.expect(result.length).to.equal(1)
+    chai.expect(result[0]).to.have.test
+    chai.expect(result[0].test).to.equal('val')
+  });
+
+  /**
+   * coreUtils.array.contains
+   * 
+   */
+  it('coreUtils.array.contains()', function() {
+    var testArray = ["test0", "test1", "test2"];
+    var result = coreUtils.array.contains(testArray, 'test1');
+    chai.expect(result).to.be.true
+    var result2 = coreUtils.array.contains(testArray, 'test3');
+    chai.expect(result2).to.be.false
+  });
+
+});
