f40bff709d3d2004e9e5e94262acef69b98e8d57
gregorybesson <gregory.besson@adfab.fr>
2016-12-26 09:57:56 +0100

    ic: refactoring the e2e tests to be based on xpath

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -22,15 +22,16 @@ describe('Abe', function() {
 
     it('Create a single article', function(client) {
       client
+        .useXpath()
         .url('http://localhost:3003/abe/editor')
-        .waitForElementVisible('body')
+        .waitForElementVisible('//body')
         .assert.title('Abe')
-        .click('select[id="level-1"] option:nth-child(2)')
-        .click('select[name="selectTemplate"] option[value="single"]')
-        .waitForElementVisible('div[data-precontrib-templates=single]', 1000)
-        .setValue('div[data-precontrib-templates=single] > div > input', 'ftest')
-        .click('button[type="submit"]')
-        .waitForElementVisible('form[id="abeForm"]', 2000)
+        .click('//*[@id="level-1"]/option[2]')
+        .click('//*[@id="selectTemplate"]/option[8]')
+        .waitForElementVisible("//div[@data-precontrib-templates='single']//input[@id='name']", 1000)
+        .setValue("//div[@data-precontrib-templates='single']//input[@id='name']", 'ftest')
+        .click("//button[@type='submit']")
+        .waitForElementVisible("//*[@id='abeForm']", 2000)
         .assert.urlEquals("http://localhost:3003/abe/editor/articles/ftest.html", "Clicked URL Matches with URL of the New Window");
     });
 
@@ -38,7 +39,7 @@ describe('Abe', function() {
       client
         .useXpath()
         .url('http://localhost:3003/abe/editor')
-        .waitForElementVisible('//body')
+        .waitForElementVisible("//body")
         .pause(1000)
         .assert.containsText("//table[@id='navigation-list']/tbody/tr[1]/td[2]/a", "/articles/ftest.html");
     });
