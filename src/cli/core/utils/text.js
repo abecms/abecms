e88ca4ede1266c426313b7dcd82151a94a0158dd
wonknu10 <fabrice.labbe@adfab.fr>
2017-03-23 15:02:56 +0100

    fix: using () or [] on tab name break tab Fixes #160
    some character breaks tab navigation because twitter boostrap throw JS error

--- src/cli/core/utils/text.js
+++ src/cli/core/utils/text.js
@@ -1,8 +1,9 @@
 import xss from 'xss'
 
-export function replaceUnwantedChar(str) {
+export function replaceUnwantedChar(str, additionnalChar = null) {
   var chars = {'’': '', '\'': '', '"': '', 'Š': 'S', 'š': 's', 'Ž': 'Z', 'ž': 'z', 'À': 'A', 'Á': 'A', 'Â': 'A', 'Ã': 'A', 'Ä': 'A', 'Å': 'A', 'Æ': 'A', 'Ç': 'C', 'È': 'E', 'É': 'E', 'Ê': 'E', 'Ë': 'E', 'Ì': 'I', 'Í': 'I', 'Î': 'I', 'Ï': 'I', 'Ñ': 'N', 'Ò': 'O', 'Ó': 'O', 'Ô': 'O', 'Õ': 'O', 'Ö': 'O', 'Ø': 'O', 'Ù': 'U', 'Ú': 'U', 'Û': 'U', 'Ü': 'U', 'Ý': 'Y', 'Þ': 'B', 'ß': 'Ss', 'à': 'a', 'á': 'a', 'â': 'a', 'ã': 'a', 'ä': 'a', 'å': 'a', 'æ': 'a', 'ç': 'c', 'è': 'e', 'é': 'e', 'ê': 'e', 'ë': 'e', 'œ': 'oe', 'ì': 'i', 'í': 'i', 'î': 'i', 'ï': 'i', 'ð': 'o', 'ñ': 'n', 'ò': 'o', 'ó': 'o', 'ô': 'o', 'õ': 'o', 'ö': 'o', 'ø': 'o', 'ù': 'u', 'ú': 'u', 'û': 'u', 'ý': 'y', 'þ': 'b', 'ÿ': 'y'}
   for(var prop in chars) str = str.replace(new RegExp(prop, 'g'), chars[prop])
+  if(additionnalChar != null) for(var prop in additionnalChar) str = str.replace(new RegExp(prop, 'g'), additionnalChar[prop])
   return str
 }
 
