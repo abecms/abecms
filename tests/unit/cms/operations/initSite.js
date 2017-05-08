085a35b7832732199d426e01282a8524f8f10fe1
gregorybesson <gregory.besson@adfab.fr>
2017-05-08 10:05:36 +0200

    ic: additional test

--- tests/unit/cms/operations/initSite.js
+++ tests/unit/cms/operations/initSite.js
@@ -17,6 +17,7 @@ var cmsOperations = require('../../../../src/cli').cmsOperations
 var cmsTemplates = require('../../../../src/cli').cmsTemplates
 var Manager = require('../../../../src/cli').Manager;
 var Page = require('../../../../src/cli').Page;
+var User = require('../../../../src/cli').User;
 
 describe('cmsOperations', function() {
 
@@ -34,6 +35,48 @@ describe('cmsOperations', function() {
         sinon.assert.callCount(stubadd, 7)
         stubadd.restore()
 
+        done()
+      }.bind(this));
+  });
+
+  it('cmsOperations.initSite updateSecurity()', function(done) {
+    // stub
+    const s = sinon.sandbox.create();
+    const create = new cmsOperations.initSite()
+    const stubConf = s.stub(config, 'save', function (p) { return p; }.bind(this));
+    const stubRead = s.stub(User.manager.instance, 'read');
+    const stubUpdate = s.stub(User.manager.instance, 'update');
+    const uadd = s.stub(User.operations, 'add')
+    const uactivate = s.stub(User.operations, 'activate')
+    uadd.returns({user:{id:1}})
+    uactivate.returns(true)
+    stubRead.returns({})
+    stubUpdate.returns(true)
+
+    const u = {
+      'username': 'nametest',
+      'name': 'nametest',
+      'email': 'email',
+      'password': 'password',
+      'role': {
+        'workflow':'admin',
+        'name':'Admin'
+      }
+    }
+    
+    create.updateSecurity({security:true, username:'nametest', email:'email', password:'password'})
+      .then(function(resSave) {
+
+        sinon.assert.callCount(stubConf, 1)
+        uadd.calledWith(u)
+        sinon.assert.callCount(uadd, 1)
+        sinon.assert.callCount(uactivate, 1)
+
+        stubConf.restore()
+        uadd.restore()
+        uactivate.restore()
+        stubRead.restore()
+        stubUpdate.restore()
         done()
       }.bind(this));
   });
