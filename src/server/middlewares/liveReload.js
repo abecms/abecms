688503ef4f17a2300005e017c9229afa639a169b
gregorybesson <gregory.besson@adfab.fr>
2017-07-13 13:57:59 +0200

    fix: These middleware should insert html content only on html pages

--- src/server/middlewares/liveReload.js
+++ src/server/middlewares/liveReload.js
@@ -11,11 +11,15 @@ var middleware = function(req, res, next) {
   if( req.url.indexOf('/abe/') > -1) {
     var send = res.send
     res.send = function (string) {
-      var body = string instanceof Buffer ? string.toString() : string
-      body = body.replace(/<\/body>/g, function (w) {
-        return '<script src="/abecms/libs/livereload.js?snipver=1&host=localhost&port='+port+'"></script>\n<script>document.addEventListener("LiveReloadDisconnect", function() {\nsetTimeout(function() {\nwindow.top.location.reload()\n}, 2000);\n})\n</script>' + w
-      })
-      send.call(this, body)
+      if(typeof string === 'string' || string instanceof Buffer){
+        var body = string instanceof Buffer ? string.toString() : string
+        body = body.replace(/<\/body>/g, function (w) {
+          return '<script src="/abecms/libs/livereload.js?snipver=1&host=localhost&port='+port+'"></script>\n<script>document.addEventListener("LiveReloadDisconnect", function() {\nsetTimeout(function() {\nwindow.top.location.reload()\n}, 2000);\n})\n</script>' + w
+        })
+        send.call(this, body)
+      } else {
+        send.call(this, string)
+      }
     }
     next()
   } else {
--- src/server/middlewares/login.js
+++ src/server/middlewares/login.js
@@ -1,15 +1,19 @@
 var middleware = function(req, res, next) {
-  if( req.url.indexOf('/abe/') > -1) {
+  if( req.url.indexOf('/abe/') > -1 && req.url.indexOf('/abe/rest/') < 0) {
     var send = res.send
     res.send = function (string) {
-      var body = string instanceof Buffer ? string.toString() : string
-      body = body.replace(/<\/body>/g, function (w) {
-        return '<input type=\'hidden\' id=\'globalCsrfToken\' value=\'' + res.locals._csrf + '\' /><script src=\'/abecms/scripts/user-login-compiled.js\'></script>' + w
-      })
-      body = body.replace(/<\/form>/g, function (w) {
-        return '<input type=\'hidden\' name=\'_csrf\' value=\'' + res.locals._csrf + '\' />' + w
-      })
-      send.call(this, body)
+      if(typeof string === 'string' || string instanceof Buffer){
+        var body = string instanceof Buffer ? string.toString() : string
+        body = body.replace(/<\/body>/g, function (w) {
+          return '<input type=\'hidden\' id=\'globalCsrfToken\' value=\'' + res.locals._csrf + '\' /><script src=\'/abecms/scripts/user-login-compiled.js\'></script>' + w
+        })
+        body = body.replace(/<\/form>/g, function (w) {
+          return '<input type=\'hidden\' name=\'_csrf\' value=\'' + res.locals._csrf + '\' />' + w
+        })
+        send.call(this, body)
+      } else {
+        send.call(this, string)
+      }
     }
     // res.locals.csrfToken = token
     next()
