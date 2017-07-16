62a48bd0571c4fffa6d20c75f0f2a1ceacfe3c59
gregorybesson <gregory.besson@adfab.fr>
2017-07-16 12:41:42 +0200

    ic: 23rd e2e test online

--- tests/e2e/data/data2.js
+++ tests/e2e/data/data2.js
@@ -20,41 +20,41 @@ describe('Abe', function() {
       done();
     });
 
-    // it('Create a data template post', function(client) {
-    //   client
-    //     .useXpath()
-    //     .url('http://localhost:3003/abe/editor')
-    //     .click('//*[@id="selectTemplate"]/option[3]')
-    //     .waitForElementVisible("//div[@data-precontrib-templates='data']//input[@id='name']", 1000)
-    //     .setValue("//div[@data-precontrib-templates='data']//input[@id='name']", 'data')
-    //     .click("//button[@type='submit']")
-    //     .pause(1000)
-    //     .waitForElementVisible('//*[@id="abeForm"]', 2000)
-    //     .assert.urlEquals("http://localhost:3003/abe/editor/data.html", "Clicked URL Matches with URL of the New Window");
-    // });
+    it('Create a data template post', function(client) {
+      client
+        .useXpath()
+        .url('http://localhost:3003/abe/editor')
+        .click('//*[@id="selectTemplate"]/option[3]')
+        .waitForElementVisible("//div[@data-precontrib-templates='data']//input[@id='name']", 1000)
+        .setValue("//div[@data-precontrib-templates='data']//input[@id='name']", 'data')
+        .click("//button[@type='submit']")
+        .pause(1000)
+        .waitForElementVisible('//*[@id="abeForm"]', 2000)
+        .assert.urlEquals("http://localhost:3003/abe/editor/data.html", "Clicked URL Matches with URL of the New Window");
+    });
 
-    // it('Abe type data reference json', function(client) {
-    //   client
-    //     .useXpath()
-    //     .url('http://localhost:3003/abe/editor/data.html')
-    //     .waitForElementVisible('//body')
-    //     .pause(1000)
-    //     .expect.element("//form[@id='abeForm']//li[@class='active']/a").text.to.contain('slug');
-    // });
+    it('Abe type data reference json', function(client) {
+      client
+        .useXpath()
+        .url('http://localhost:3003/abe/editor/data.html')
+        .waitForElementVisible('//body')
+        .pause(1000)
+        .expect.element("//form[@id='abeForm']//li[@class='active']/a").text.to.contain('slug');
+    });
     
     
-    // it('The autocomplete article is deleted in the manager', function(client) {
-    //   client
-    //     .useXpath()
-    //     .url('http://localhost:3003/abe/editor')
-    //     .waitForElementVisible('//body')
-    //     .pause(1000)
-    //     .click("//table[@id='navigation-list']/tbody/tr[1]/td[7]/div/a")
-    //     .pause(1000)
-    //     .acceptAlert()
-    //     .url('http://localhost:3003/abe/editor')
-    //     .pause(2000)
-    //     .expect.element("//table[@id='navigation-list']/tbody/tr[1]/td[2]/a").text.to.not.contain('/articles/ftest.html');
-    // });
+    it('The autocomplete article is deleted in the manager', function(client) {
+      client
+        .useXpath()
+        .url('http://localhost:3003/abe/editor')
+        .waitForElementVisible('//body')
+        .pause(1000)
+        .click("//table[@id='navigation-list']/tbody/tr[1]/td[7]/div/a")
+        .pause(1000)
+        .acceptAlert()
+        .url('http://localhost:3003/abe/editor')
+        .pause(2000)
+        .expect.element("//table[@id='navigation-list']/tbody/tr[1]/td[2]/a").text.to.not.contain('/articles/ftest.html');
+    });
   });
 });
