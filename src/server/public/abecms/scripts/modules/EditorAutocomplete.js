238d2fe48ecde47567f6e076211809daf4f0dc5c
nicolaslabbe <nicolas.labbe@adfab.fr>
2017-10-19 10:20:23 +0200

    feature
    All relative url for abe type data
    ```
    {{abe type='data' key='test' source='/abe/plugin/plugin-name/route-get?q='}}
    ```

--- src/server/public/abecms/scripts/modules/EditorAutocomplete.js
+++ src/server/public/abecms/scripts/modules/EditorAutocomplete.js
@@ -438,7 +438,7 @@ export default class EditorAutocomplete {
         }
       }
 
-      if (dataVal.indexOf('http') === 0) {
+      if(/http:\/\/|https:\/\/|\/\/|(\?(.*)=)/.test(dataVal)) { 
         this._ajax(
           {
             url: `${dataVal}${val}`,
